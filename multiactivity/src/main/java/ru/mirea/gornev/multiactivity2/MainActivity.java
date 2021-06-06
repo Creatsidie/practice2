8bf584681b8416d2a6e4813959996592387147a2
Creatsidie <gornev.01@mail.ru>
2021-06-06 11:50:56 +0300

    practice2

---
+++ multiactivity/src/main/java/ru/mirea/gornev/multiactivity2/MainActivity.java
@@ -0,0 +1,25 @@
+package ru.mirea.gornev.multiactivity2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView edo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickNewActivity(View view) {
+        edo = (TextView) findViewById(R.id.textView);
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("key", edo.getText());
+        startActivity(intent);
+    }
+}
